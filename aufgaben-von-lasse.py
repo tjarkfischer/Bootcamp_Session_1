0e03ea13c4f5a904dbc19a7887fb2231aca5f85f
LAFENSKE <lassemads.fenske@otto.de>
2024-09-13 13:48:09 +0200

    Erstellung von aufgaben file

--- aufgaben-von-lasse.py
+++ aufgaben-von-lasse.py
@@ -1,6 +1,10 @@
-"""userName = input("Enter your name: ")
+#Aufgaben 1-3
+userName = input("Enter your name: ")
 userAge = int(input("Enter your age: "))
-print(f"My name ist {userName}.\nI am {userAge} years old.")"""
+print(f"My name ist {userName}.\nI am {userAge} years old.")
+
+
+#check how many upper and lower case characters are in text
 test = input("")
 lower = 0
 upper = 0
@@ -9,4 +13,4 @@ for i in test:
         lower += 1
     elif i.isupper():
         upper += 1
-print(lower, " ", upper)
+print("lower case: ", lower, "upper case: ", upper)
