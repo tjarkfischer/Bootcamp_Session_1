fbecd1e562b15fb5c42b576fbba7c6852224b66c
Tom <tom.rietzke@otto.de>
2024-09-13 13:43:21 +0200

    neu

--- Stringculator.py
+++ Stringculator.py
@@ -1,12 +1,20 @@
 #tom
+from sys import excepthook
+
+
 def main():
+
     x = input("Zahl 1: ")
     y = input("Zahl 2: ")
+    try:
+        x = int(x)
+    except ValueError:
+        x = 0
+    try:
+        y = int(y)
+    except ValueError:
+        y = 0
 
-    if x == "":
-        x = "0"
-    if y == "":
-        y = "0"
     print(stringculator(x, y))
 
 
