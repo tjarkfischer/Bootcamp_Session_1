2cb896f557405b8ee0360f7654249c479af801d7
LAFENSKE <lassemads.fenske@otto.de>
2024-09-13 12:56:54 +0200

    Erstellung von aufgaben file

--- aufgaben-von-lasse.py
+++ aufgaben-von-lasse.py
@@ -1,3 +1,12 @@
-userName = input("Enter your name: ")
+"""userName = input("Enter your name: ")
 userAge = int(input("Enter your age: "))
-print(f"My name ist {userName}.\nI am {userAge} years old.")
+print(f"My name ist {userName}.\nI am {userAge} years old.")"""
+test = input("")
+lower = 0
+upper = 0
+for i in test:
+    if i.islower():
+        lower += 1
+    elif i.isupper():
+        upper += 1
+print(lower, " ", upper)
